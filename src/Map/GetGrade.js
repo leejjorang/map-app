5d5174d676d03e2b85596b395401a481cf7b794a
gh38 <gahee3389@pukyong.ac.kr>
2023-11-13 02:39:39 +0900

    수정 및 추가

---
+++ src/Map/GetGrade.js
@@ -0,0 +1,32 @@
+var grade = null;
+
+export function getGrade(Loc){
+    
+    var currentLoc = Loc;
+    
+    if(currentLoc){	
+	    
+        var center = currentLoc;
+	    var start = [center[0]-74.6837539486587, center[1]-83.64580442244187];		// 좌측 하단 좌표
+	    var end = [center[0]+74.6837539486587, center[1]+83.64580442244187];		// 우측 상단 좌표
+	    var I = 250;
+	    var J = 280;
+		
+	    function fetchGrade(){      // grade 데이터 구하기
+
+            const url = `https://geo.safemap.go.kr/geoserver/safemap/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&BBOX=${start[0]},${start[1]},${end[0]},${end[1]}&CRS=EPSG:3857&WIDTH=500&HEIGHT=560&LAYERS=A2SM_CRMNLHSPOT_TOT&STYLES=&FORMAT=image/png&QUERY_LAYERS=A2SM_CRMNLHSPOT_TOT&INFO_FORMAT=application/json&I=${I}&J=${J}`;
+		    
+		    fetch(url)
+		    .then((response) => response.json())
+		    .then((jsonData) => {
+			    grade = jsonData.features[0].properties.GRAD;
+		    })
+		    .catch((error) => {   // 에러 or 범죄 주의 구간을 벗어난 경우 ( ex. 건물 안)
+                //console.log("error: ",error);
+                grade = "";
+		    });
+	    };
+    fetchGrade();
+	}	
+    return grade;
+}
--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import MapContext from './MapContext'
 import CategoryList from './CategoryList.js';
+import { getGrade } from './GetGrade.js';
 import 'ol/ol.css'
 import './Map.css'
-import { Map as OlMap, View, Feature } from 'ol'
-import { defaults as defaultControls, FullScreen, ScaleLine } from 'ol/control'
+import { Map as OlMap, View, Feature, Overlay } from 'ol'
+import { defaults as defaultControls, FullScreen, ScaleLine, Zoom, ZoomSlider } from 'ol/control'
 import { fromLonLat, get as getProjection } from 'ol/proj'
-import { Tile as TileLayer, Vector } from 'ol/layer'
+import { Tile as TileLayer, Vector, Marker } from 'ol/layer'
 import { XYZ, TileWMS, Vector as VectorWMS } from 'ol/source'
-import { Style, Circle, Fill, Stroke } from 'ol/style'
+import { Style, Circle, Fill, Stroke, Icon } from 'ol/style'
 import { Point } from 'ol/geom'
-import {
-  DragRotateAndZoom,
-  defaults as defaultInteractions,
-} from 'ol/interaction'
+import { DragRotateAndZoom, defaults as defaultInteractions} from 'ol/interaction'
+import $, { get } from 'jquery';
 
-//import * as FetchData from './FetchData.js';
-
+//import {fetchData} from './FetchData.js';
 //FetchData.js - 원하는 데이터 추출 파일 (현재는 return값 지정x)
-//FetchData.fetchData(); - 추출
+//fetchData(); - 추출
 
 const safeKey = process.env.REACT_APP_SAFE_KEY;
 const mapKey = process.env.REACT_APP_MAP_KEY;
@@ -68,7 +66,15 @@ function initMap(){
   })
 
   map.addControl(new ScaleLine());
+  map.addControl(new Zoom());
   getCurrentLocation();
+  
+  onoffWMS(); // 오동작 방지
+
+  $('#culocation').on('click', function(){
+    map.getView().setCenter(currentLoc);
+    map.getView().setZoom(17);
+  });
 }
 
 const satelliteMap = new TileLayer({
@@ -99,7 +105,15 @@ const handleHybridButtonClick = () => {
   map.removeLayer(hybridMap);
 };
 
+const handleZoomInClick = () => {
+  const zoom = map.getView().getZoom() + 1;
+  map.getView().setView(zoom);
+};
 
+const handleZoomOutClick = () => {
+  const zoom = map.getView().getZoom() - 1;
+  map.getView().setView(zoom);
+};
 
 // 3. 현 위치
 
@@ -129,7 +143,7 @@ function updateMap(position){
 
       currentMarker = new Feature({
         geometry: new Point(currentLoc),
-        name: "CurrentLocation"
+        name: "CurrentLocation",
       });
       
       currentMarker.setStyle(iconStyle);
@@ -139,6 +153,7 @@ function updateMap(position){
           features: [currentMarker]
         })
       });
+      vectorLayer.setZIndex(10);
 
       map.addLayer(vectorLayer);	// 현위치 마커
       map.getView().setCenter(currentLoc); // 현재 위치로 지도 중심 업데이트
@@ -149,57 +164,130 @@ function updateMap(position){
 }
 
 // 4. 레이어 onoff
-
-var checkNum = 0;
-
-function onoffWMS(){
-  if(checkNum % 2 === 0){
-    document.getElementsByClassName('switch_label').innerHTML='<div>off</div>'
+function onoffWMS(){  
+  if(document.querySelector('#switch').checked === true){
     map.addLayer(wmsLayer);
-  }else if(checkNum % 2 === 1){
-    document.getElementsByClassName('switch_label').innerHTML='<div>on</div>'
+  }else if(document.querySelector('#switch').checked === false){
     map.removeLayer(wmsLayer);
   }
-  checkNum++;
 }
-
 
 // 일정 간격 체크
 
 var grade = null;
 
-setInterval(function(){
-	if(currentLoc){
-		
-		var center = currentLoc;
-		var start = [center[0]-74.6837539486587, center[1]-83.64580442244187];		// 좌측 하단 좌표
-		var end = [center[0]+74.6837539486587, center[1]+83.64580442244187];		// 우측 상단 좌표
-		var I = 250;
-		var J = 280;
-		
-    // grade 데이터 구하기
-		function fetchGrade(){
+// setInterval(() => {
+//   grade = getGrade(currentLoc);
+//   console.log(grade);
+// }, 50000); // 50초
 
-			const url = `https://geo.safemap.go.kr/geoserver/safemap/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&BBOX=${start[0]},${start[1]},${end[0]},${end[1]}&CRS=EPSG:3857&WIDTH=500&HEIGHT=560&LAYERS=A2SM_CRMNLHSPOT_TOT&STYLES=&FORMAT=image/png&QUERY_LAYERS=A2SM_CRMNLHSPOT_TOT&INFO_FORMAT=application/json&I=${I}&J=${J}`;
-			//console.log(url);
+function getAddress(){
 
-			fetch(url)
-			.then((response) => response.json())
-			.then((jsonData) => {
-				//displayData(jsonData);
-        grade = jsonData.features[0].properties.GRAD
-			})
-			.catch((error) => {   // 에러 or 범죄 주의 구간을 벗어난 경우 ( ex. 건물 안)
-				// 가장 최근 등급으로 유지하는 코드 필요
-        //console.log("error: ",error);
-			});
-		};
+  var iconStyle = new Style({
+    image: new Icon({
+      src:'https://map.vworld.kr/images/ol3/marker_blue.png'
+    })
+  });
+  // var iconStyle = new Style({
+  //   image: new Circle({
+  //     radius: 6,
+  //     fill: new Fill({color: 'blue'}),
+  //     stroke: new Stroke({color: 'white', width: 3})
+  //   })
+  // });
+  // searchMarker.setStyle(iconStyle);
 
-		fetchGrade();
-	}
-	
-}, 5000); // 5초
+  var srchSource = new VectorWMS();
+  var srchLayer = new Vector({
+    source : srchSource
+  });
 
+  $.ajax({  // ADDRESS
+        url: "https://api.vworld.kr/req/search?",
+        type: "GET",
+        dataType: "jsonp",
+        data: {
+          service: "search",
+          request: "search",
+          version: "2.0",
+          crs: "EPSG:900913",
+          bbox: "14300071.146077,4160339.6527027,14450071.146077,4210339.6527027",
+          size: "20",
+          page: "1",
+          query: $('#searchInput').val(),
+          type: "place",
+          format: "json",
+          errorformat: "json",
+          key: mapKey
+        },
+        success: function (data) {
+          if(data.response.status === "NOT_FOUND"){
+      // 빈 곳 처리
+    }else{
+      console.log(data);
+      for(var i=0; i<(data.response.result.items).length; i++){
+
+        var datalists = data.response.result.items[i];
+
+        if(i===0){
+          map.getView().setCenter([datalists.point.x*1, datalists.point.y*1]);
+          map.getView().setZoom(17);
+        }
+
+        var marker = new Feature({
+          geometry: new Point([datalists.point.x, datalists.point.y]),
+          name: "searchedLocation",
+          title: datalists.title,
+          content: datalists.address.road,
+        });
+        
+        marker.setStyle(iconStyle);
+        srchSource.addFeature(marker);
+      }
+
+      srchLayer.setZIndex(15);
+      map.addLayer(srchLayer);
+      
+      
+      var popup = new Overlay({
+        element: document.querySelector('#popup'),
+        positioning: 'bottom-center',
+        stopEvent: false,
+        offset: [0, -50],
+      });
+
+    
+      map.addOverlay(popup);
+
+      map.on('click', function(evt){    // 클릭 중첩 현상
+
+        // 클릭된 위치에서 마커 찾기
+        var feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+          return feature;
+        });
+
+        if(feature){
+          console.log(document.getElementById('popup-content'));
+          var coordinates = feature.getGeometry().getCoordinates();
+      
+          var title = feature.get('title');
+          var content = feature.get('content');
+
+          document.querySelector('#popup-content').innerHTML=('<h2>' + title + '</h2><p>' + content + '</p>');
+          console.log(document.getElementById('popup-content'));
+          popup.setPosition(coordinates);
+        }else{  
+          // 마커 밖을 선택하면 레이어 삭제
+          popup.setPosition(undefined);
+          map.removeLayer(srchLayer);
+        }
+      });
+    }
+  }
+});
+}
+
+ 
 const Map = ({ children }) => {
   const [mapObj, setMapObj] = useState({})
 
@@ -207,12 +295,68 @@ const Map = ({ children }) => {
     
     //Map 객체 생성 및 vworld 지도 설정
     initMap();
-    
-    document.getElementById('culocation').addEventListener('click', () => {
-      map.getView().setCenter(currentLoc);
-      map.getView().setZoom(17);
-    });
 
+    // $.ajax({ 값 하나
+    //       url: "https://api.vworld.kr/req/address?",
+    //       type: "GET",
+    //       dataType: "jsonp",
+    //       data: {
+    //         service: "address",
+    //         request: "GetCoord",
+    //         version: "2.0",
+    //         crs: "EPSG:4326",
+    //         type: "ROAD",
+    //         address: "오설록",
+    //         format: "json",
+    //         errorformat: "json",
+    //         key: mapKey
+    //       },
+    //       success: function (result) {
+    //         console.log(result);
+    //       }
+    //     })
+
+    // $.ajax({  // ADDRESS
+    //       url: "https://api.vworld.kr/req/search?",
+    //       type: "GET",
+    //       dataType: "jsonp",
+    //       data: {
+    //         service: "search",
+    //         request: "search",
+    //         version: "2.0",
+    //         crs: "EPSG:900913",
+    //         bbox: "14300071.146077,4160339.6527027,14450071.146077,4210339.6527027",
+    //         size: "10",
+    //         page: "1",
+    //         query: "해운대로",
+    //         type: "address",
+    //         category: "road",
+    //         format: "json",
+    //         errorformat: "json",
+    //         key: mapKey
+    //       },
+    //       success: function (result) {
+    //         console.log(result);
+    //       }
+    //     })
+
+    // $.ajax({
+    //         url: "https://map.vworld.kr/search.do?",
+    //         type: "GET",
+    //         dataType: "jsonp",
+    //         data:{
+    //         category:"poi",
+    //         q:"푸라닭",
+    //         pageunit: "200",
+    //         output:'json',
+    //         pageindex:'50',
+    //         apiKey: mapKey,
+    //         },
+    //     success: function (result) {
+    //           console.log(result);
+    //         }
+    //       })
+      
     /* RN이랑 통신 시 사용
     document.getElementById('button').addEventListener('click', () => {
       const message = {key1:'GRADE', key2:grade};
@@ -234,14 +378,14 @@ const Map = ({ children }) => {
       <nav>
         <div className="nav-container">
           <div className="switch_wrapper">
-            <input type="checkbox" id="switch" className="chkbox" onChange={onoffWMS}/>
+            <input type="checkbox" id="switch" className="chkbox" onChange={onoffWMS} defaultChecked={false}/>
             <label htmlFor="switch" className="switch_label">
               <span className="btn"></span>
             </label>
           </div>
           <div className="search-container">
             <input type="text" id="searchInput" placeholder="주소 검색..." />
-            <button id="searchBtn">검색</button>
+            <button id="searchBtn" onClick={getAddress}>검색</button>
           </div>
           <button className="nav-safe">안전<br />길찾기</button>
         </div>
@@ -251,6 +395,9 @@ const Map = ({ children }) => {
         <div className="content" style={{width:"100%", height:"100%"}}>
           <MapContext.Provider className="inner" value={mapObj}>
             {children}
+            <div id="popup" className='ol-popup'>
+              <div id="popup-content">==</div>
+            </div>
           </MapContext.Provider>
         </div>
       <CategoryList></CategoryList>
